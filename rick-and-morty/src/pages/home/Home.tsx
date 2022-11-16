5cdf231bf7edfd8320789bd2b43928617c7f086b
Brendon Costa <100797380+brendoncosta-alianca@users.noreply.github.com>
2022-11-16 19:23:28 -0300

    create and implement Home component

--- rick-and-morty/src/pages/home/Home.tsx
+++ rick-and-morty/src/pages/home/Home.tsx
@@ -1,5 +1,31 @@
+import style from "./Home.module.css";
+
 export default function Home() {
     return (
-        <h1>Rick And Morty</h1>
+        <div className={style.home_content}>
+            <div>
+                <h1 className={style.test}>Rick And Morty</h1>
+            </div>
+            <div className={style.page_content}>
+                <div className={style.d_card}>
+                    <div className={style.content}>
+                        <h2 className={style.heading}>Location</h2>
+                        <p className={style.data_content}>View all locations in the series.</p>
+                    </div>
+                </div>
+                <div className={style.d_card}>
+                    <div className={style.content}>
+                        <h2 className={style.heading}>Character</h2>
+                        <p className={style.data_content}>View all characters from the series.</p>
+                    </div>
+                </div>
+                <div className={style.d_card}>
+                    <div className={style.content}>
+                        <h2 className={style.heading}>Episode</h2>
+                        <p className={style.data_content}>View all episodes of the series.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
