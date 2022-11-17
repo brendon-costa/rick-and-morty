bcb3a3c8793cbfb31caa544b4fbc099f2c675b0f
Brendon Costa <100797380+brendoncosta-alianca@users.noreply.github.com>
2022-11-17 20:17:25 -0300

    create card in character screen

--- rick-and-morty/src/pages/character/Character.tsx
+++ rick-and-morty/src/pages/character/Character.tsx
@@ -1,5 +1,43 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import {Box, CardActionArea, Container, Grid} from "@mui/material";
+
 export default function Character() {
+
+    const list = [1,2,3,4,5,6];
+
     return (
-        <h1>Character</h1>
+        <Container>
+            <h1>Character</h1>
+            <Grid container>
+                {list.map(value => {
+                    return (
+                        <Grid xs={12} sm={6} md={4} key={value} style={{marginBottom: '35px', display: 'flex', justifyContent: 'center'}}>
+                            <Card sx={{ maxWidth: 300 }}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        height="250"
+                                        image="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+                                        alt="green iguana"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            Lizard
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                                            species, ranging across all continents except Antarctica
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        </Container>
     )
 }
