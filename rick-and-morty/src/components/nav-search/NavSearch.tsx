fd7109afed1cd2cb0a908f98b4989e8f9c7ce7f0
Brendon Costa <100797380+brendoncosta-alianca@users.noreply.github.com>
2022-11-21 18:10:09 -0300

    implement open modal in advanced search

--- rick-and-morty/src/components/nav-search/NavSearch.tsx
+++ rick-and-morty/src/components/nav-search/NavSearch.tsx
@@ -1,7 +1,18 @@
 import style from './NavSearch.module.css';
-import {AppBar, Box, Button, Container, TextField, Typography} from "@mui/material";
+import {
+    AppBar,
+    Button,
+    Dialog, DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    TextField,
+    Typography
+} from "@mui/material";
 import { useForm } from "react-hook-form";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import Grid from '@mui/material/Unstable_Grid2';
+
 
 interface NavSearchModel {
     title: string;
@@ -10,11 +21,16 @@ interface NavSearchModel {
 
 export function NavSearch(prop: NavSearchModel) {
 
+    const [openModal, setOpenModal] = useState(false);
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     useEffect(() => {
         const value = watch("search");
         prop.changeSearch(value);
-    }, [watch("search")])
+    }, [watch("search")]);
+
+    const openAndCloseModal = () => {
+        setOpenModal(!openModal);
+    }
 
     return (
         <AppBar position="static" className={style.navSearchContainer}>
@@ -28,11 +44,40 @@ export function NavSearch(prop: NavSearchModel) {
                         {...register("search", { required: true })}
                     />
                 </form>
-                <Button variant="text" size="small" className="direction-col">
+                <Button variant="text" size="small" className="direction-col" onClick={openAndCloseModal}>
                     <span>Advanced</span>
                     <span>search</span>
                 </Button>
             </div>
+            <Dialog
+                open={openModal}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    Advanced search
+                </DialogTitle>
+                <form>
+                    <DialogContent>
+                        <Grid xs={12} sm={6} md={4} columnSpacing={2}>
+                            <TextField
+                                label="idade" placeholder="Enter a name" variant="outlined" size="small"
+                                {...register("search", { required: true })}
+                            />
+                            <TextField
+                                label="teste" placeholder="Enter a name" variant="outlined" size="small"
+                                {...register("search", { required: true })}
+                            />
+                        </Grid>
+                    </DialogContent>
+                </form>
+                <DialogActions>
+                    <Button onClick={openAndCloseModal}>Cancel</Button>
+                    <Button onClick={() => console.log('foi')} autoFocus>
+                        Search
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </AppBar>
     )
 }
